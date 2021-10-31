d10507b93ebc94399b285afca28185c974e30aeb
Ahmed <ahmedahmani01@gmail.com>
2021-10-31 23:07:29 +0100

    Added ServicesSection

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Fragment } from "react/cjs/react.development";
 import AboutSection from "../Components/AboutSection";
-
+import ServicesSection from "../Components/ServicesSection";
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
 };
 
 export default AboutUs;
